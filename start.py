c0ef9b7fb18ec31300c67d620871a5a186ee9139
Jeff Wheeless <jeffrey.wheeless@traderinteractive.com>
2021-07-12 17:08:48 -0400

    Refactored code, updated code, made it so the line can move around smoother and calcs the difference between lines and takes that into consideration when random moving. still has some spikes while running that should be normalized.

--- start.py
+++ start.py
@@ -1,5 +1,7 @@
 import pyautogui
 import random
+# frameinfo.filename, frameinfo.lineno
+from inspect import currentframe, getframeinfo
 
 # pyautogui.moveTo(100,100,duration=0.5)
 
@@ -21,17 +23,33 @@ def getAltCoord(suggestedCoord, coord):
 
 
 def getPartialPoint(coord, current, total):
-    randNumber = 1
-    print(total)
+    # get an inbetween point between the current and end location
+    randNumber = random.randint(3, 5)
+    # frameinfo = getframeinfo(currentframe())
+    # print("(" + str(frameinfo.lineno) + ") total: " + str(total))
+    diff = current - coord
+    if (coord > current):
+        diff = coord - current
+
+    # randNumber = random.randint(5, 10)
+    # if (diff/50 > 2):
+    #     # totalDivided = round(total/3, 0)+1
+    #     randNumber = random.randint(5, 10)
     if (total > 5):
         totalDivided = round(total/3, 0)+1
         if (totalDivided <= 0):
             totalDivided = 1
         randNumber = random.randint(1, totalDivided)
 
+    # print(randNumber)
+
     partialEndPoint = coord - ((coord - current)/randNumber)
     if (current > coord):
         partialEndPoint = coord - ((current - coord)/randNumber)
+
+    frameinfo = getframeinfo(currentframe())
+    print("(" + str(frameinfo.lineno) + ") Next endpoint: " +
+          str(partialEndPoint))
 
     return round(partialEndPoint)
 
@@ -46,25 +64,41 @@ def drawLine(x, y, linearX, LinearY):
     # pyautogui.leftClick(x, y, 0, 0.05)
     pyautogui.dragTo(x, y, button='left')
     pyautogui.moveTo(x, y)
-    print(str(x) + ', ' + str(y))
+    frameinfo = getframeinfo(currentframe())
+    print("(" + str(frameinfo.lineno) + ") current Location: " +
+          str(x) + ', ' + str(y))
     pyautogui.leftClick(x, y, 0, 0.05)
 
 
-def realignCoordBeforePlot(coord, diffMethod, endpoint, randInt):
+def realignCoordBeforePlot(coord, diffMethod, endpoint, randInts):
+    randInt = random.randint(randInts[0], randInts[1])
+    frameinfo = getframeinfo(currentframe())
+    print("(" + str(frameinfo.lineno) + ") randInt: " + str(randInt))
     if (diffMethod == 'add' and coord <= endpoint):
-        coord = coord + random.randint(round(randInt/2), randInt)
+        coord = coord + randInt
     elif (diffMethod == 'add' and coord >= endpoint):
-        coord = coord - random.randint(round(randInt/2), randInt)
+        coord = coord - randInt
 
     if (diffMethod == 'subtract' and coord >= endpoint):
-        coord = coord - random.randint(round(randInt/2), randInt)
+        coord = coord - randInt
     elif (diffMethod == 'subtract' and coord <= endpoint):
-        coord = coord + random.randint(round(randInt/2), randInt)
+        coord = coord + randInt
 
     return coord
 
 
-def mouseMove(coordKeyPrime, coordKeySecond, startRandInt, endRandInt, endPoint):
+def getDiffByCoord(coord1, coordLast):
+    randIntFromDifference = coordLast - coord1
+    # if (coord1 < coordLast):
+    #     randIntFromDifference = coordLast - coord1
+
+    frameinfo = getframeinfo(currentframe())
+    print("(" + str(frameinfo.lineno) + ") Difference: " +
+          str(randIntFromDifference))
+    return randIntFromDifference
+
+
+def mouseMove(loc1, loc2, coordKeyPrime, coordKeySecond, startRandInt, endRandInt, endPoint):
     randIntRange = [5, 10]
     start = pyautogui.position()[coordKeyPrime]
     endPrimeLoc = endPoint[coordKeyPrime] - \
@@ -82,36 +116,88 @@ def mouseMove(coordKeyPrime, coordKeySecond, startRandInt, endRandInt, endPoint)
         endPrimeLoc = endPrimeLoc - \
             random.randint(randIntRange[0], randIntRange[1])
 
+    xdiff = 'add'
+    if (loc1[0] > loc2[0]):
+        xdiff = 'subtract'
+
+    ydiff = 'add'
+    if (loc1[1] > loc2[1]):
+        ydiff = 'subtract'
+
+    endPrimeLoc = int(round(endPrimeLoc, 0))
+
     slope = (loc2[1] - loc1[1]) / (loc2[0] - loc1[0])
     intercept = loc1[1] - (slope * loc1[0])
-    for x in range(start, endPrimeLoc, random.randint(startRandInt, endRandInt)):
+    for x in range(
+        int(start),
+        int(endPrimeLoc),
+        random.randint(startRandInt, endRandInt)
+    ):
         current = pyautogui.position()
         # left off here, thinking i need to make the next set of randoms have a range of the difference of current and end point divided by a number
-        randIntFromDifference = current[coordKeySecond] - loc2[coordKeySecond]
+        # getDiffByCoord(coord1, coordLast)
+        differenceY = getDiffByCoord(
+            current[coordKeySecond], loc2[coordKeySecond])
+
+        if (differenceY < 0):
+            frameinfo = getframeinfo(currentframe())
+            print("(" + str(frameinfo.lineno) +
+                  ")randIntFromDifference is negative, error")
+        randIntBase = differenceY / 2
+        if(randIntBase >= 25):
+            randInts = [10, 25]
+        elif(randIntBase <= -25):
+            randInts = [-25, -10]
+        # if (differenceY <= 5):
+        #     randInts = [-3, 3]
+        # elif (differenceY <= 10):
+        #     randInts = [-5, 5]
+        # elif (differenceY <= 20):
+        #     randInts = [-10, 10]
+        else:  # if (differenceY > 10):
+            if (randIntBase < 0):
+                randInts = [round(randIntBase/2, 0), round(randIntBase, 0)]
+            elif (randIntBase >= 0):
+                randInts = [round(randIntBase/2, 0), round(randIntBase, 0)]
         # realignCoordBeforePlot(coord, diffMethod, endpoint, randInt)
-        if (coordKeySecond == 1):
+
+        frameinfo = getframeinfo(currentframe())
+        print("(" + str(frameinfo.lineno) +
+              ") Rand Ints: " + str(randInts))
+        y = current[coordKeySecond]
+        if (coordKeySecond == 0):
+            y = realignCoordBeforePlot(current[coordKeySecond], xdiff,
+                                       endPoint[coordKeySecond], randInts)
+        else:
             y = realignCoordBeforePlot(current[coordKeySecond], ydiff,
-                                       endPoint[coordKeySecond], randIntFromDifference)
-        elif (coordKeySecond == 0):
-            x = realignCoordBeforePlot(current[coordKeySecond], xdiff,
-                                       endPoint[coordKeySecond], randIntFromDifference)
+                                       endPoint[coordKeySecond], randInts)
 
         if (y == current[0] or y == current[1]):
-            y = y + random.randint(-15, 15)
+            # y = y + random.randint(-15, 15)
+            y = y + random.randint(randInts[0], randInts[1])
         if (x == current[0] or x == current[1]):
-            x = x + random.randint(-15, 15)
+            if (xdiff == "add"):
+                x = x + random.randint(1, 15)
+            if (xdiff != "add"):
+                x = x + random.randint(-15, -1)
+            # x = x + random.randint(randInts[0], randInts[1])
+
+        # if (ydiff == 'add' and current[coordKeySecond] < endPoint[coordKeySecond]):
+        #     y = y + random.randint(15, 30)
 
         if (coordKeyPrime == 0):
             straight = (slope * current[coordKeyPrime]) + intercept
             getAltCoord(y, straight)
             drawLine(x, y, current[coordKeyPrime], straight)
         else:
+            if(slope <= 0):
+                slope = 1
             straight = (current[coordKeyPrime] - intercept) / slope
             getAltCoord(x, straight)
             drawLine(y, x, straight, current[coordKeyPrime])
 
 
-def getThere(total):
+def getThere(loc1, loc2, total):
     randIntRange = [25, 50]
     # if (total > 4):
     #     randIntRange = [
@@ -125,13 +211,15 @@ def getThere(total):
         getPartialPoint(loc2[0], current[0], total),
         getPartialPoint(loc2[1], current[1], total)
     ]
-    if(random.randint(1, 10) >= 3):
-        mouseMove(0, 1, randIntRange[0], randIntRange[1], partialEndPoint)
-    else:
-        mouseMove(1, 0, randIntRange[0], randIntRange[1], partialEndPoint)
+    # if(random.randint(1, 10) >= 3):
+    mouseMove(loc1, loc2, 0, 1,
+              randIntRange[0], randIntRange[1], partialEndPoint)
+    # else:
+    # mouseMove(loc1, loc2, 1, 0,
+    #           randIntRange[0], randIntRange[1], partialEndPoint)
 
 
-def moveThere(total):
+def moveThere(loc1, loc2, total):
     current = pyautogui.position()
     if (
         (
@@ -141,12 +229,14 @@ def moveThere(total):
     ):
         pyautogui.dragTo(loc2[0], loc2[1], button='left')
         pyautogui.moveTo(loc2[0], loc2[1], 0.05, pyautogui.easeInElastic)
-        print(pyautogui.position())
+        frameinfo = getframeinfo(currentframe())
+        print("(" + str(frameinfo.lineno) + ") Current Pos: " +
+              str(pyautogui.position()))
         total = 10
     else:
         total = total - 1
-        getThere(total)
-        moveThere(total)
+        getThere(loc1, loc2, total)
+        moveThere(loc1, loc2, total)
 
 
 # loc1 = [932, 216] # top right
@@ -161,14 +251,6 @@ foo = input("Mouse over second position ")
 loc2 = pyautogui.position()
 print(loc2)
 
-xdiff = 'add'
-if (loc1[0] > loc2[0]):
-    xdiff = 'subtract'
-
-ydiff = 'add'
-if (loc1[1] > loc2[1]):
-    ydiff = 'subtract'
-
 runs = input("Runs: ")
 pyautogui.moveTo(loc1[0], loc1[1])
 pyautogui.leftClick(loc1[0], loc1[1])
@@ -181,7 +263,7 @@ for one in range(0, int(runs)):
     total = 10
     pyautogui.moveTo(loc1[0], loc1[1])
     currentstart = pyautogui.position()
-    moveThere(total)
+    moveThere(loc1, loc2, total)
 
 
 # for x in range(loc1[0], loc2[0], random.randint(50, 100)):

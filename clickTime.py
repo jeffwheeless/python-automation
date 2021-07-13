29638ab57aa2d0e81ca4a29d46beab80b9ea82aa
Jeff Wheeless <jeffrey.wheeless@traderinteractive.com>
2021-07-13 16:30:31 -0400

    Updated clickTime to where a middle button press will reset the time for the overall average making it so your averages wont get messed up between movements and things.

--- clickTime.py
+++ clickTime.py
@@ -22,10 +22,16 @@ def on_click(x, y, button, pressed):
         global totalTime
         global totalClicks
         global previousTime
+
         currentTime = time.time()
         print('\nMouse clicked at ({0}, {1}) with {2}'.format(x, y, button))
         timeTaken = currentTime - previousTime
         print("Recent Click took: " + str(timeTaken))
+        if(str(button) == "Button.middle"):
+            totalTime = 0
+            totalClicks = 0
+            print("Clearing")
+
         if (totalClicks <= 1):
             totalTime = 0
         else:

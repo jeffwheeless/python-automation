150163c97eeff26ae03a5b737649795a3f1166cf
Jeff Wheeless <jeffrey.wheeless@traderinteractive.com>
2021-07-14 12:26:04 -0400

    Updated file distanceBetweenPoints so it will run forever until exited

--- distanceBetweenPoints.py
+++ distanceBetweenPoints.py
@@ -1,8 +1,6 @@
 import pyautogui
-import random
-import time
 
-for one in range(0, 100):
+while 1 == 1:
     foo = input("Mouse over first position ")
     loc1 = pyautogui.position()
     print(loc1)
@@ -11,5 +9,5 @@ for one in range(0, 100):
     loc2 = pyautogui.position()
     print(loc2)
 
-    print("Distance: " + str(loc1[0]-loc2[0]) + ", " + str(loc1[1]-loc2[1]))
-    foo = input("<1> to run again")
+    print("Distance: " + str(loc1[0]-loc2[0]) +
+          ", " + str(loc1[1]-loc2[1]) + "\n")

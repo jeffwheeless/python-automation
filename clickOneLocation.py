ad5f00231df6ad6b6eaa59447deb1e3ab0bf988f
Jeff Wheeless <jeffrey.wheeless@traderinteractive.com>
2021-07-20 16:59:47 -0400

    click on location script changed so its friendlier

--- clickOneLocation.py
+++ clickOneLocation.py
@@ -3,14 +3,13 @@ import random
 import time
 
 
-def performLeftClickSleep(loc1):
+def performLeftClick(loc1):
     pyautogui.leftClick(
-        random.randint(loc1[0]-10, loc1[0]+10),
-        random.randint(loc1[1]-10, loc1[1]+10),
+        None,
+        None,
         0,
         random.uniform(0.3, 0.7)
     )
-    sleepRandom(0.5, 0.9)
 
 
 def sleepRandom(smallInt, largeInt):
@@ -22,16 +21,22 @@ def sleepRandom(smallInt, largeInt):
 foo = input("Mouse over first position ")
 loc1 = pyautogui.position()
 print(loc1)
-foo = input("Mouse over first position ")
+# foo = input("Mouse over first position ")
 
 for x in range(1, 120):
     print(x)
-    pyautogui.moveTo(
-        random.randint(loc1[0]-10, loc1[0]+10),
-        random.randint(loc1[1]-10, loc1[1]+10),
-        random.uniform(0.3, 0.7)
-    )
-    sleepRandom(0.9, 1.4)
-    performLeftClickSleep(loc1)
-    performLeftClickSleep(loc1)
-    sleepRandom(30.6, 50.5)
+    current = pyautogui.position()
+    if (
+        (current[0] >= loc1[0]+12 or current[0] <= loc1[0]-12) and
+        (current[1] >= loc1[1]+12 or current[1] <= loc1[1]-12)
+    ):
+        pyautogui.moveTo(
+            random.randint(loc1[0]-10, loc1[0]+10),
+            random.randint(loc1[1]-10, loc1[1]+10),
+            random.uniform(0.3, 0.7)
+        )
+        sleepRandom(0.9, 1.4)
+    performLeftClick(loc1)
+    sleepRandom(0.3, 0.7)
+    performLeftClick(loc1)
+    sleepRandom(30.6, 52.5)

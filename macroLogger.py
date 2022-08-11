4043b96d9f9d3a6a6a746c5933d9f642ed76ffaa
Jeff Wheeless <jeffrey.wheeless@traderinteractive.com>
2022-08-11 12:16:41 -0400

    Changed mouseLocationOutput to be macroLogger to reflect what it does better.

--- macroLogger.py
+++ macroLogger.py
@@ -1,30 +1,28 @@
 import pyautogui
+import helper
 
+config = helper.read_config()
 locationRuns = input("How many run locations: ")
 locationRuns = int(locationRuns)
 loc = []
 print("Apped to postion input to add key")
 print("alttab to Alt+Tab otherwise write in key")
-with open('mouseLocationOutput.txt', 'w') as f:
+with open('macroLogger.txt', 'w') as f:
     f.write("\n")
-for temp in range(0, locationRuns):
 
-    with open('mouseLocationOutput.txt', 'a') as f:
+for temp in range(0, locationRuns):
+    with open('macroLogger.txt', 'a') as f:
         extraKey = input("Mouse over " + str(temp+1) + " position ")
         current = pyautogui.position()
         distVariation = input("Distance variation between clicks ")
         if (distVariation == ""):
-            distVariation = "3"
+            distVariation = config['Defaults']['distVariation']
 
         timeVariation = input("Time between clicks ")
         if (timeVariation == ""):
-            timeVariation = "1"
+            timeVariation = config['Defaults']['timeVariation']
 
         loc.append([current[0], current[1], extraKey,
                     int(distVariation), int(timeVariation)])
-        # loc1 = pyautogui.position()
-        # loc1 = [3323, 1086]
-        # loc1 = [2800, 700]
-        # loc1 = [500, 0]
         print(loc[temp])
         f.write("loc.append(" + str(loc[temp]) + ")\n")

d7be48362ac2e47850e1f747d5991236bd7c4d99
Jeff Wheeless <jeffrey.wheeless@traderinteractive.com>
2022-09-15 16:14:51 -0400

    Made changes to speed up clickclick

--- clickClick.py
+++ clickClick.py
@@ -12,33 +12,40 @@ from modules.inventory import Inventory
 x = []
 y = []
 
-y.append([1058, 405, '', 8, 31.5])
-x.append([1236, 582, '', 8, 31.5])
-y.append([1057, 876, '', 8, 31.5])
-x.append([913, 635, '', 8, 31.5])
+y.append([3371, 917, '', 10, 5])
+x.append([3528, 1094, '', 10, 5])
+y.append([3371, 1319, '', 10, 5])
+x.append([3220, 1109, '', 10, 5])
 loc = [1479, 916, '', 5, 30]
 lock = False
 total = 0
 totalTimeStamped = 0
+low = 1000000
+high = 0
 
+# def getRandom(smallInt, largeInt):
+    
 
 def sleepRandom(smallInt, largeInt):
-    sleep = random.uniform(smallInt, largeInt)
+    # sleep = random.uniform(smallInt, largeInt)
+    sleep = random.random()*.1
+    demicalPlace = .1
+    # sleep = random.random()
     if (sleep > 10):
         print("Long sleep of: " + str(sleep))
 
     time.sleep(sleep)
     # time.sleep(random.uniform(smallInt, largeInt))
 
-    if (random.randint(1, 1000) > 900):
+    if (random.random()*10 > 8):
         sleepRandom(0, 0.5)
 
-    if (sleep > 0.5):
-        if (random.randint(1, 1000) > round(925-largeInt)):
-            sleepRandom(1, 2)
+    # if (sleep > 0.5):
+    #     if (random.randint(1, 1000) > round(925-largeInt)):
+    #         sleepRandom(1, 2)
 
-        if (random.randint(1, 1000) > round(960-largeInt)):
-            sleepRandom(1, 3)
+    #     if (random.randint(1, 1000) > round(960-largeInt)):
+    #         sleepRandom(1, 3)
 
         # if (random.randint(1, 1000) > round(990-largeInt)):
         #     sleepRandom(1, 20)
@@ -79,61 +86,88 @@ def on_scroll(x, y, dx, dy):
     current = pyautogui.position()
     loc = [current[0], current[1], loc[2], loc[3]]
     # sleepRandom(1, 2)
-
+    
+def modifyNumber(number, modify):
+    modifiedNumber = number-int(random.random()*modify)
+    if (random.random() > 0.5):
+        modifiedNumber = number+int(random.random()*modify)
+        
+    return modifiedNumber
 
 def on_press(key):
     global loc
     global lock
     global total, totalTimeStamped
+    global low, high
     startTime = 0
-    if key == keyboard.Key.up:
-        current = pyautogui.position()
-        loc = [current[0], current[1], loc[2], loc[3]]
-        print("Setting New Location")
+    if key == keyboard.Key.caps_lock:
+        startTime = time.time()
+        if (lock == False):
+            current = pyautogui.position()
+            pyautogui.leftClick(current[0], current[1], _pause=False)
+            closestX = x[0]
+            if (abs(current[0]-x[0][0]) > abs(current[0]-x[1][0])):
+                closestX = x[1]
+                
+            closestY = y[0]
+            if (abs(current[1]-y[0][1]) > abs(current[1]-y[1][1])):
+                closestY = y[1]
+                
+            closestAxis = 'x'
+            if (abs(current[0]-closestX[0]) > abs(current[1]-closestY[1])):
+                closestAxis = 'y'
+            
+            
+            if closestAxis == 'y':
+                modloc = [
+                    modifyNumber(current[0], closestY[3]),
+                    modifyNumber(closestY[1], closestY[3]),
+                ]
+            elif closestAxis == 'x':
+                modloc = [
+                    modifyNumber(closestX[0], closestX[3]),
+                    modifyNumber(current[1], closestX[3]),
+                ]
+            time.sleep(random.random()*.1)
+            pyautogui.leftClick(modloc[0], modloc[1], _pause=False)
+            time.sleep(random.random()*.1)
+            pyautogui.moveTo(modifyNumber(current[0], 5), modifyNumber(current[1], 5))
 
     if key == keyboard.Key.down:
         startTime = time.time()
         if (lock == False):
             current = pyautogui.position()
-            pyautogui.leftClick(current[0], current[1])
-            locationDetermine = random.randint(1, 4)
+            pyautogui.leftClick(current[0], current[1], _pause=False)
+            closestX = x[0]
+            if (abs(current[0]-x[0][0]) > abs(current[0]-x[1][0])):
+                closestX = x[1]
+                
+            closestY = y[0]
+            if (abs(current[1]-y[0][1]) > abs(current[1]-y[1][1])):
+                closestY = y[1]
+                
+            closestAxis = 'x'
+            if (abs(current[0]-closestX[0]) > abs(current[1]-closestY[1])):
+                closestAxis = 'y'
             
-            # modloc = [
-            #     random.randint(current[0]-y[1][3], current[0]+y[1][3]),
-            #     random.randint(y[1][1]-y[1][3], y[1][1]+y[1][3]),
-            # ]
-            if locationDetermine == 1:
+            
+            if closestAxis == 'y':
                 modloc = [
-                    random.randint(x[0][0]-x[0][3], x[0][0]+x[0][3]),
-                    random.randint(current[1]-x[0][3], current[1]+x[0][3]),
+                    modifyNumber(current[0], closestY[3]),
+                    modifyNumber(closestY[1], closestY[3]),
                 ]
-            elif locationDetermine == 2:
+            elif closestAxis == 'x':
                 modloc = [
-                    random.randint(x[1][0]-x[1][3], x[1][0]+x[1][3]),
-                    random.randint(current[1]-x[1][3], current[1]+x[1][3]),
+                    modifyNumber(closestX[0], closestX[3]),
+                    modifyNumber(current[1], closestX[3]),
                 ]
-            elif locationDetermine == 3:
-                modloc = [
-                    random.randint(current[0]-y[0][3], current[0]+y[0][3]),
-                    random.randint(y[0][1]-y[0][3], y[0][1]+y[0][3]),
-                ]
-            elif locationDetermine == 4:
-                modloc = [
-                    random.randint(current[0]-y[1][3], current[0]+y[1][3]),
-                    random.randint(y[1][1]-y[1][3], y[1][1]+y[1][3]),
-                ]
-            # pyautogui.moveTo()
-            # mouseMove(current[0], current[1], modloc[0], m0odloc[1])
-            # sleepRandom(0.01, 0.05)
-            pyautogui.leftClick(modloc[0], modloc[1])
-            # sleepRandom(0.01, 0.05)
-
-            modlocCurrent = [
-                random.randint(current[0]-5, current[0]+5),
-                random.randint(current[1]-5, current[1]+5),
-            ]
-            pyautogui.moveTo(modlocCurrent[0], modlocCurrent[1])
-            # mouseMove(modloc[0], modloc[1], current[0], current[1])
+            if (random.random() > 0.7):
+                time.sleep(random.random()*0.5)
+                pyautogui.leftClick(modloc[0], modloc[1], _pause=False)
+            time.sleep(random.random()*0.5)
+            pyautogui.leftClick(modloc[0], modloc[1], _pause=False)
+            time.sleep(random.random()*0.5)
+            pyautogui.moveTo(modifyNumber(current[0], 5), modifyNumber(current[1], 5))
 
     if key == keyboard.Key.right:
         lock = True if lock == False else False
@@ -153,9 +187,14 @@ def on_press(key):
     if (startTime != 0): 
         total = total + 1
         runTimeStamped = (time.time()-startTime)
+        if (runTimeStamped < low):
+            low = runTimeStamped
+        if (runTimeStamped > high):
+            high = runTimeStamped
         totalTimeStamped = totalTimeStamped + runTimeStamped
         averageTimeStamped = totalTimeStamped/total
-        print("Iteration took: " + str(runTimeStamped) + "s   \t| Average Time Stamped took: " + str(averageTimeStamped) + "s")
+        print(f"Iteration took: {runTimeStamped}s   \t| Average Time Stamped took: {averageTimeStamped}s")
+        print(f"High: {high}s   \t| Low: {low}s")
 
 
 listener = keyboard.Listener(on_press=on_press)
